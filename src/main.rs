532f7802adda66383d5c4b41dde7d4b2ca376cfa
Francososa <franky.sosa@gmail.com>
2022-04-19 15:47:44 -0600

    Implemente draw trait for different types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use guilly::Draw;
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Drawing button with label {}", self.label);
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "Drawing select box with options {:#?}", 
+            self.options
+        );
+    }
 }
